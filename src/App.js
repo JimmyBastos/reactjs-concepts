9c35c2263f724b404f263f560a6c7e022fff5747
Jimmy Bastos <jimmybastosdev@gmail.com>
2020-06-19 07:57:28 -0300

    feat: new repository form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import "./styles.css";
 
 function App() {
   let [repositoryList, setRepositoryList] = useState([]);
+  let [formData, setFormData] = useState({
+    title: "",
+    url: "",
+    techs: ""
+  });
 
   useEffect(() => {
     api.get("repositories").then(({ data }) => {
@@ -12,11 +17,19 @@ function App() {
     })
   }, [])
 
-  async function handleAddRepository() {
+  function handleInputChange(prop) {
+    return (event) => {
+      setFormData({ ...formData, [prop]: event.target.value })
+    }
+  }
+
+  async function handleAddRepository(event) {
+    event.preventDefault()
+    event.stopPropagation()
+
     let payload = {
-      title: "New repository - " + (new Date()).toLocaleTimeString(),
-      url: "https://spartandev.com.br/repolist",
-      techs: ["react", "node", "javascript"],
+      ...formData,
+      techs: formData.techs.split(","),
     }
 
     let { data: repository } = await api.post(`repositories`, payload)
@@ -31,10 +44,37 @@ function App() {
   }
 
   return (
-    <div>
-      <ul data-testid="repository-list">
+    <div className="container">
+      <form className="repository-form" onSubmit={handleAddRepository}>
+        <h3>Novo Repositório</h3>
+        <input
+          type="text"
+          placeholder="Titulo"
+          required
+          value={formData.title}
+          onChange={handleInputChange("title")}
+        />
+
+        <input type="text"
+          placeholder="Url"
+          required
+          value={formData.url}
+          onChange={handleInputChange("url")}
+        />
+
+        <input
+          type="text"
+          placeholder="Tecnologias (Ex: React.js, React Native, Node.js)"
+          value={formData.techs}
+          onChange={handleInputChange("techs")}
+        />
+
+        <button>Adicionar</button>
+      </form>
+
+      <ul className="repository-list" data-testid="repository-list">
         {repositoryList.map(repository => (
-          <li key={repository.id}>
+          <li className="repository" key={repository.id}>
             {repository.title}
 
             <button onClick={() => handleRemoveRepository(repository.id)}>
@@ -43,7 +83,6 @@ function App() {
           </li>
         ))}
       </ul>
-      <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
 }
